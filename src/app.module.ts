7f70c7664e4736f99f10c0322006564cd4858213
Julio Cid <julio.cid@reign.cl>
2022-07-15 08:09:52 -0400

    remove fatify and add dotenv
    add lock json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongodbModule } from './mongodb/mongodb.module';
 
 @Module({
-  imports: [MongodbModule],
+  imports: [
+    MongodbModule,
+    ConfigModule.forRoot({
+      envFilePath: 'development.env',
+      isGlobal: true,
+      cache: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
